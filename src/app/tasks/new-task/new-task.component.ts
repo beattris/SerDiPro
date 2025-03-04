eb767852d0e32bae5edf9d8a53f3bab20b7c7504
beattris <beatriceebirim@gmail.com>
2025-03-04 14:37:19 +0100

    created a service and injected it to the components that need it

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -8,10 +9,13 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
+
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  private tasksService = inject(TasksService);
 
   onAddTask(title: string, description: string) {
+    this.tasksService.addTask({title, description})
     this.formEl()?.nativeElement.reset();
   }
 }
